c13b63de9025e06bf88e111a16c89617ad30bb56
yuki0920 <ywsep20@gmail.com>
2024-03-23 10:56:22 +0900

    Improve prompt

--- hotcook-linebot/gpt.py
+++ hotcook-linebot/gpt.py
@@ -5,8 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
-CHAT_UPDATE_INTERVAL_SEC = 1
-OPENAI_API_TEMPERATURE = 0.5
+OPENAI_API_TEMPERATURE = 0.1
 # OPENAI_API_MODEL="gpt-3.5-turbo-0125"
 OPENAI_API_MODEL="gpt-4-0125-preview"
 
@@ -20,10 +19,15 @@ def call_gpt(recipe):
 
     prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", """あなたはレシピ探しが得意な優秀なシェフです。入力として与えられた素材やメニューを元に、類似したホットクックメニューを探し、下記のフォーマットで回答してください。
+        ("system", """あなたはレシピ探しが得意な優秀なシェフです。入力として与えられた素材やメニューを元に、ホットクックのレシピを探し、下記のフォーマットで回答してください。
         質問内容に素材やメニュー以外の情報が含まれている場合は、自然に返答した後に、「素材やメニューを教えてくれたら、もっと具体的なレシピを探せるかもしれないよ。」と返答してください。
         回答には、口語体で回答してください。
         メニューを見つけられない場合は、「ごめんね、<メニュー or 食材>に関するレシピは見つからなかったよ。」と回答してください。
+        入力例は適宜変換してください。例えば、カレーとカレーライスは同じメニューとして扱ってください。無水カレーもカレーとして扱ってください。
+
+        入力例:
+        メニューの場合の例: カレー、カレーライス、無水カレー
+        素材の場合の例: 豚肉、牛肉、玉ねぎ
 
         フォーマット:
         【メニュー: <メニュー名>】
